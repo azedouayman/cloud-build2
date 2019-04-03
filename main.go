a05c389b0bcb0544d2bf7e2322db789533c9b225
aymanazedou <aymanazedou@gmail.com>
2019-04-03 17:18:56 +0200

    Update main.go

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
         log.Printf("Serving request: %s", r.URL.Path)
         host, _ := os.Hostname()
         fmt.Fprintf(w, "Hello, world!\n")
-        fmt.Fprintf(w, "Version: 3.0.0\n")
+        fmt.Fprintf(w, "Version: 4.0.0\n")
         fmt.Fprintf(w, "Hostname: %s\n", host)
 }
 // [END all]
